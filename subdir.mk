f9ddbe02cc58925f7fb446c320db4b9069a0d7e2
Dennis Droese <droesedj@msoe.edu>
2018-04-15 18:00:12 -0500

    moved makefile into main directory

---
+++ subdir.mk
@@ -0,0 +1,54 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+./circle.cpp \
+./gcontext.cpp \
+./image.cpp \
+./line.cpp \
+./main.cpp \
+./matrix.cpp \
+./point.cpp \
+./poly.cpp \
+./shape.cpp \
+./triangle.cpp \
+./x11context.cpp 
+
+OBJS += \
+./circle.o \
+./gcontext.o \
+./image.o \
+./line.o \
+./main.o \
+./matrix.o \
+./point.o \
+./poly.o \
+./shape.o \
+./triangle.o \
+./x11context.o 
+
+CPP_DEPS += \
+./circle.d \
+./gcontext.d \
+./image.d \
+./line.d \
+./main.d \
+./matrix.d \
+./point.d \
+./poly.d \
+./shape.d \
+./triangle.d \
+./x11context.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o: ./%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -O2 -g -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
